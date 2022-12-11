2e0dd0783a8c80ef57c0ca908a6918c440efa613
Sergey <volshibenik@gmail.com>
2022-12-11 12:58:17 +0000

    feat: add material, fix lint errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
+// eslint-disable-next-line @angular-eslint/prefer-on-push-component-change-detection
 @Component({
-  selector: 'app-root',
+  selector: 'vol-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'portfolio';
